97a78f52b103ee14908562d27bd26440fde63cb4
chris901003 <a0987999103.gmail.com>
2021-11-12 16:12:46 +0800

    TLE_MLE_RE

---
+++ Dispatch/source/1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	// vector<int>c(10000000,0);
+	// vector<int>b(10000000,0);
+	// vector<int>a(10000000,0);
+	// for(int i=0;i<10000000000;i++){}
+	vector<int>a(10);
+	assert(false);
+	cout << "Hello C++" << endl;
+	return 0;
+}
