3005bdc8b566d8967ce27275768408bfb8ea539f
ChenHongJyun <47174380+ChenHongJyun@users.noreply.github.com>
2021-11-26 12:17:12 +0800

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,14 @@ from app import db
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
+relations = db.Table(
+    'relations',
+    db.Column('topic_id', db.Integer, db.ForeignKey('topic.topic_id')),
+    db.Column('problem_id', db.Integer, db.ForeignKey('problem.problem_id'))  
+)
+#✔
+
+
 class User(db.Model):
     __tablename__ = "user"
     user_id = db.Column(db.Integer, primary_key=True)
@@ -43,11 +51,11 @@ class Problem(db.Model):
     upload_date = db.Column(db.DateTime, nullable=False)
     #upload_date做法同上
     problem_to_user = db.relationship("User_problem", backref="problem")
-    #problem_topics = db.relationship("Topic", secondary=relations, backref="Problem")
+    problem_topics = db.relationship("Topic", secondary=relations, backref="Problem")
 
     def __repr__(self):
         return f"Problem('{self.problem_id}', '{self.problem_name}', '{self.problem_content}', '{self.difficulty}')"
-#!
+#✔
 
 class User_problem(db.Model):
     __tablename__ = "user_problem"
@@ -58,23 +66,18 @@ class User_problem(db.Model):
 
 #✔
 
-relations = db.Table(
-    'relations',
-    db.Column('topic_id', db.Integer, db.ForeignKey('topic.topic_id')),
-    db.Column('problem_id', db.Integer, db.ForeignKey('problem.problem_id'))  
-)
-#✔
+
 
 class Topic(db.Model):
     __tablename__ = "topic"
     topic_id = db.Column(db.Integer, primary_key=True)
     topic_name = db.Column(db.String(30), unique=True, nullable=False)
-    #topic_problems = db.relationship("Problem", secondary=relations, backref="Topic")
+    topic_problems = db.relationship("Problem", secondary=relations, backref="Topic")
 
     def __repr__(self):
         return f"Topic('{self.topic_id}', '{self.topic_name}')"
-#!
 
+#✔
 
 
 class Submission(db.Model):
@@ -84,34 +87,38 @@ class Submission(db.Model):
     problem_id = db.Column(db.Integer, db.ForeignKey(Problem.problem_id), nullable=False)
     source_id = db.Column(db.Integer, nullable=False)
     status = db.Column(db.Integer, nullable=False)
-    # error_hint = db.Column(db.Integer)              # storage mode
-    # error_line = db.Column(db.Integer)              # storage mode
+    error_hint = db.Column(db.Integer)              
+    error_line = db.Column(db.Integer)              
     language = db.Column(db.String(20), nullable=False)
     time_used = db.Column(db.String(20), nullable=False)      
     Memory_used = db.Column(db.String(20), nullable=False)     
     exam_id = db.Column(db.Integer)
     homework_id = db.Column(db.Integer)
-    upload_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    upload_date = db.Column(db.DateTime, nullable=False)
+    #同registwer_date做法
     code_content = db.Column(db.String(524288), nullable=False)
 
     def __repr__(self):
         return f"Submission('{self.submission_id}', '{self.user_id}', '{self.problem_id}', '{self.status}', '{self.code_content}')"
-
+#✔
+    
 class Queue(db.Model):
     __tablename__ = "queue"
     source_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(User.user_id), nullable=False)
     problem_id = db.Column(db.Integer, db.ForeignKey(Problem.problem_id), nullable=False)
     mode = db.Column(db.Integer, nullable=False, default=0)
-    status = db.Column(db.Integer, nullable=False)
-    submission_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    #utc is American time!!
-    #exam_id = db.Column(db.Integer, nullable=False)      # is_nullable
-    #homework_id = db.Columm(db.Integer, nullable=False)  # is_nullable
-    code_content = db.Column(db.String(524288), nullable=False)
+    exam_id = db.Column(db.Integer)      
+    homework_id = db.Columm(db.Integer)  
     languege = db.Column(db.String(20), nullable=False)
-
+    upload_date = db.Column(db.DateTime, nullable=False)
+    #同registwer_date做法
+    
+    #status = db.Column(db.Integer, nullable=False)
+    #code_content = db.Column(db.String(524288), nullable=False)
+    
+    
     def __repr__(self):
         return f"Queue('{self.submission_id}', '{self.user_id}', '{self.problem_id}', '{self.status}', '{self.code_content}','{self.languege}')"
+#✔
 
-#!
