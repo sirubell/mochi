85dde56bab45d1a132f5c600f2ea3c2ba07e9f83
ChenHongJyun <47174380+ChenHongJyun@users.noreply.github.com>
2021-11-26 15:26:12 +0800

    Update models.py

--- backend/flask-api.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Api, Resource
-from app import api
-
-class problem(Resource):
-    def get(self):
-        return {"string":"Hello_world"}
-
-api.add_resource(problem, "/problem")
--- backend/test.py
+++
@@ -1,6 +0,0 @@
-from app import db
-from models import User, Problem, Topic, User_problem, Submission, Queue
-# from fuction import add_submission, delete_submission, refresh_submission_status, add_queue, delete_queue_and_get_data, add_problem_topic, delete_problem_topic, new_user, delete_user_problem, new_problem, delete_problem, show_user_problem, show_problem_submission, show_user_submission, show_problem_set
-db.drop_all()
-db.create_all()
-db.session.commit()
