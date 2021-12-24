ab6cde9338bad4d0403acbc660c760ee42658c04
LKV <a23202418@gmail.com>
2021-12-24 13:47:27 +0800

    XD

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,4 @@
 from backend import app
 
 if __name__=="__main__":
-<<<<<<< HEAD
     app.run(debug=True,host="192.168.122.232",port='5000') 
-=======
-    #app.run(debug=True,host="192.168.122.232",port='8000') 
-    app.run(debug=True)
->>>>>>> ad711218e9e7286465ed167e2ba8b39850dd3876
