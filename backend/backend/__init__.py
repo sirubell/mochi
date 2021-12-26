4481093c7d3d5055e712af98bfa604a9ca3ad08f
LKV <a23202418@gmail.com>
2021-12-26 21:25:59 +0800

    ?

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -20,7 +20,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 login_manager.login_message = '該網頁需要登入才能瀏覽'
-BASE = 'c\\Users\\a2320\\Desktop\\coding\\mochi\\backend\\'
+BASE = 'c/Users/a2320/Desktop/coding/mochi/backend/'
 
 app.permanent_session_lifetime
 
