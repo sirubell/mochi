5ad5debf8745bd35671f4e4726dc52ea7b29cf6a
CHJ <koalaho091698@gmail.com>
2021-12-31 11:36:53 +0800

    space

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -38,6 +38,7 @@ class User(db.Model, UserMixin):
     def get_reset_token(self, expires_sec = 1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
+    
     @staticmethod
     def verify_reset_token(token):
         s = Serializer(app.config['SECRET_KEY'])
