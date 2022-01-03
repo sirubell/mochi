f46179fdf4cede29c734486873670501fc61a9c9
Jolie <jolie92lillian@gmail.com>
2022-01-03 22:29:17 +0800

    change

--- frontend/src/components/user/change_password.vue
+++ frontend/src/components/user/change_password.vue
@@ -15,7 +15,7 @@
             </button>
             <div class="collapse" id="dashboard-collapse">
               <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><router-link to="/reset" class="link-dark rounded">Overview</router-link></li>
+                <li><router-link to="/" class="link-dark rounded">Overview</router-link></li>
                 <li><a href="#" class="link-dark rounded">Weekly</a></li>
                 <li><a href="#" class="link-dark rounded">Monthly</a></li>
                 <li><a href="#" class="link-dark rounded">Annually</a></li>
@@ -23,9 +23,15 @@
             </div>
           </li>
           <li class="mb-1">
-            <router-link to="/change_profile" class="btn btn-toggle align-items-center rounded collapsed" data-bs-target="#orders-collapse" aria-expanded="false">
+            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
               Change Profile
-            </router-link>
+            </button>
+            <div class="collapse" id="account-collapse">
+              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                <li><router-link to="/change_email" class="link-dark rounded">Change Email</router-link></li>
+                <li><router-link to="/change_password" class="link-dark rounded">Change Password</router-link></li>
+              </ul>
+            </div>
           </li>
           <li class="border-top my-3"></li>
         </ul>
@@ -33,34 +39,49 @@
     </nav>
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <!-- <div class="justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"> -->
-        <h2 class="d-flex border-bottom">Change Profile</h2>
-        <div class="edit">
-          <div class="edit-item">
-            <span class="label d-flex">Change email :</span>
-            <input type="email" class="form-control create-input d-flex" v-model="email" placeholder="Edit email"/>
-          </div>
+      <form @submit.prevent="ChangePassword"> 
+        <h2 class="d-flex border-bottom">Change Password</h2>
+        <div class="edit">    
           <div class="edit-item">
             <span class="label d-flex">Change password :</span>
             <input type="password" class="form-control create-input d-flex" v-model="password" placeholder="Edit password"/>
           </div>
-          <div>
-            <button class="send d-flex btn btn-primary btn-block" style="margin-top:10px" @click="save">save</button>
+          <div class="edit-item">
+            <span class="label d-flex" style="margin-top:10px">Confirm password :</span>
+            <input type="password" class="form-control create-input d-flex" v-model="confirm_password" placeholder="Confirm password"/>
           </div>
+          <button class="send d-flex btn btn-primary btn-block" style="margin-top:10px" @click="save">save</button>
         </div>
-      <!-- </div> -->
+      </form>
     </main>
   </div>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name: 'Change_Profile',
+    name: 'Change_Password',
     data() {
       return {
-        email: "",
-        password: ""
+        password: '',
+        confirm_password: ''
       }
+    },
+    methods: {
+        async ChangePassword(){ //利用 try catch 作錯誤偵測
+            try{
+                const response = await axios.put('user/change_profile_password', {
+                    password: this.password,
+                    confirm_password: this.confirm_password
+                }); // 資料由後方物件帶入 
+                console.log(response);
+                this.$router.push('/user');
+            }
+            catch(e){
+                this.error = e;
+            }
+        }
     }
 }
 </script>
