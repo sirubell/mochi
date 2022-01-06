ed42e74fc8c0ccfc9b048336f98352ec5d469abc
hsu <hsuanfu0709@gmail.com>
2022-01-07 00:11:47 +0800

    1

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -6,13 +6,13 @@
       Mochi
       </router-link>
       <ul class="fs-5 nav col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><router-link to="/problem" class="nav-link px-2 link-light">Problem</router-link></li>
-        <li><router-link to="/status" class="nav-link px-2 link-light">Status</router-link></li>
-        <li><router-link to="/class/homework/all" class="nav-link px-2 link-light">Homework</router-link></li>
-        <li><router-link to="/class/exam" class="nav-link px-2 link-light">Exam</router-link></li>
-        <li><router-link to="/test-new-problem" class="nav-link px-2 link-light">New-Problem</router-link></li>
-        <li><router-link to="/test-new-exam" class="nav-link px-2 link-light" >New-Exam</router-link></li>
-        <li><router-link to="/test-new-homework" class="nav-link px-2 link-light">New Homework</router-link></li>
+        <li><router-link to="/problem" class="nav-link px-3 link-light">Problem</router-link></li>
+        <li><router-link to="/status" class="nav-link px-3 link-light">Status</router-link></li>
+        <li><router-link to="/class/homework/all" class="nav-link px-3 link-light">Homework</router-link></li>
+        <li><router-link to="/class/exam" class="nav-link px-3 link-light">Exam</router-link></li>
+        <li><router-link to="/test-new-problem" class="nav-link px-3 link-light">New-Problem</router-link></li>
+        <li><router-link to="/test-new-exam" class="nav-link px-3 link-light" >New-Exam</router-link></li>
+        <li><router-link to="/test-new-homework" class="nav-link px-3 link-light">New-Homework</router-link></li>
       </ul>
 
       <div class="fs-5 col-md-2">
